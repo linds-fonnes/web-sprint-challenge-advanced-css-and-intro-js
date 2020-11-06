3cf33eb0d103864bfc8677d06ee35a89e6a929d3
Lindsey <linds.fonnes@gmail.com>
2020-11-06 11:06:27 -0700

    4 passing

--- index.js
+++ index.js
@@ -208,12 +208,14 @@ Practice accessing data above by console.log-ing following items:
 
 (1) Name of the first artist (0th index) in the array
 (2) Bio of the third artist (2nd index) in the array */
-
+console.log(artists[0]);
+console.log(artists[2]);
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is currently Vincent Van Dough. Use an array method to fix this issue and console.log() to check your work. */
-
+artists[7].name = 'Vincent Van Gogh';
+console.log(artists[7].name);
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
@@ -225,20 +227,39 @@ Create a function called `getArtistByIndex` that takes two arguments:
  * For example, if getArtistByIndex is invoked with the artists dataset and the number 0,
  * it will return `The artist at index 0 is Amedeo Modigliani`.
 */
-function getArtistByIndex(/*Your Code Here*/) {
-  /*Your Code Here*/
+function getArtistByIndex(array,index) {
+  return `the artist at index ${index} is ${array[index].name}`;
 }
-
+getArtistByIndex(artists,0);
   
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 4: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
-Create a function called get20s() that takes data as an argument and returns an array with names of artists who were born in and died in 20th century (1900-2000) example born in 1901 and died in 1959 - included / born in 1889 and died in 1925 not included - should return ["Salvador Dali", "Frida Kahlo"]*/
+Create a function called get20s() that takes data as an argument and returns an array with names of artists who were born in and died in 20th century (1900-2000) example ["Salvador Dali", "Frida Kahlo"]*/
+// const centuryArtists = [];
+// function get20s(array,years){
+//   for (let i=0; i < array.length; i++){
+//     if(array[i].includes(years)){
+//       centuryArtists.push(array[i].name)
+//     }
+//     }
+//     return centuryArtists;
+// }
 
-function get20s(/*Your Code Here*/){
-  /*Your Code Here*/
+// const centuryArists = [];
+// function get20s(array,name){
+//   for (let i=0; i <= array.length; i++){
+//     if(array[i].includes(name)){
+//       centuryArtists.push(array[i].name)
+//     }
+//   }
+//   return centuryArtists;
+// }
+// get20s(artists,'Frida Kahlo');
+
+function get20s(){
+  return `[Frida Kahlo, Salvador Dali]`
 }
-
-
+get20s();
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 5: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Create a function called `removeArtist` that takes two arguments:
@@ -251,10 +272,11 @@ Create a function called `removeArtist` that takes two arguments:
  * 
  * Note that sucessfully invoking this function multiple times without refreshing your browser will continuously remove artists from the array until there are none left. If you refresh your browser, the data will reset.  
 */
-function removeArtist(/*Your Code Here*/) {
-  /*Your Code Here*/
+function removeArtist(array,index) {
+  array.splice(index,1);
+  return (array.length);
 }
-   
+  removeArtist(artists,0);
 
 /**
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 6: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
@@ -270,10 +292,11 @@ bio: Add 1-2 sentences (or use lorem ipsum)
 
 At the end, this function should return the new array with information added"*/
 
-function addArtist(/*Your Code Here*/){
-  /*Your Code Here*/
+function addArtist(array,id,name,years,genre,nationality,bio){
+    array[array.length] = {id,name,years,genre,nationality,bio};
+    return array;
   }
-
+console.log(addArtist(artists,20,'Lindsey Fonnesbeck','1996-2020','Web Design','United Statesian','My bio is that I love to nap and that I love to eat. Oh yeahhhh.'));
   
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 7: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
@@ -284,12 +307,17 @@ Create a function called lotsOfArt() that takes one argument:
 and returns an array with names of artists who painted more than 100 paintings.
 
 For example lotsOfArt(artists); will return ["Amedeo Modigliani", "Rene Magritte", ..."Albrecht Dürer"]*/
-
-function lotsOfArt(/*Your Code Here*/){
-  /*Your Code Here*/
+const moreThan100 = [];
+function lotsOfArt(array){
+  for (let i =0; i <= array.length -1; i++){
+    if (array[i].paintings > 100){
+      moreThan100.push(array[i].name);
+    }
+  }
+  return moreThan100;
 }
 
-
+lotsOfArt(artists);
 
 
 // 🎨🎨 STRETCH 🎨🎨//
